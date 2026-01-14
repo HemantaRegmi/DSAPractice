c039dd9c5db5fb4299802c5a909a6a84cbda8c28
HemantaRegmi <hemantaregmi20@gmail.com>
2026-01-13 21:38:05 -0600

    wip: added question description as comment for anagram. Solved solution in next commit

--- DSAPractice.js
+++ DSAPractice.js
@@ -1,2 +1,13 @@
 //Anagrams Challenge
 console.log("hello world");
+
+let word1 = "hi";
+let word2 = "hi";
+
+function validAnagram(word1, word2) {
+  for (val of word1) {
+  }
+
+  for (val of word2) {
+  }
+}
